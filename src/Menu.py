cdc7a2ba1b53bc89ef1e64586a4b20d51699e6f2
wahyuridho <saputroridho569@gmail.com>
2019-11-30 21:01:58 +0700

    First Commit

---
+++ src/Menu.py
@@ -0,0 +1,10 @@
+def menu():
+	print("Author : Wahyu Saputro / Elektro A / UTY")
+	print("-----------------------------------------------------------------")
+	print("""	---------------------------------------------------------
+	|		|Aturan Penulisan| 			|
+	|--------------------------------------------------------
+	|1.Panas 2.Sakit_Kepala 3.Bersin 4.Batuk		|
+	|5.Pilek 6.Badan_Lemas 7.Kedinginan 8.Tenggorokan_Sakit |""")
+	print("-----------------------------------------------------------------")
+
